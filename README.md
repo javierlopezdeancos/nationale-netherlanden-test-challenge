51c18fa8dbe040f78bbacb69556a4f9499b8cc27
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2025-09-18 20:17:48 +0200

    chore: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# nationale-netherlanden-test-challenge
-This challenge is part of the hiring process for some of the Software Engineer positions at Nationale Netherlanden
+# Nationale Netherlanden Test Challenge
+
+Sin cambiar la estructura del código jsx, mejora la funcionalidad de la aplicación para que:
+
+- El botón "-" disminuya el número de árboles plantados, pero no permita que el número sea menor que 1.
+- El botón "+" aumente el número de árboles plantados.
+- Para ello debes crear un botón `ChangeQuantityButton` para cada uno de los botones.
+- El componente `ChangeQuantityButton` debe recibir dos props: `label` y `onClick`.
+
+```jsx
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+        <div> - </div>
+        <div>{tree} tree</div>
+        <div> + </div>
+      <div>Plant trees €2.29</div>
+    </div>
+  );
+}
+```
+
+## Arrancar el proyecto
+
+1. Clona el repositorio
+
+```shell
+git clone git@github.com:javierlopezdeancos/nationale-netherlanden-test-challenge.git
+```
+
+2. Instala las dependencias y arranca el proyecto
+
+```shell
+npm install
+npm run dev
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/*
+Sin cambiar la estructura del código jsx, mejora la funcionalidad de la aplicación para que:
 
-function App() {
-  const [count, setCount] = useState(0)
+- El botón "-" disminuya el número de árboles plantados, pero no permita que el número sea menor que 1.
+- El botón "+" aumente el número de árboles plantados.
+- Para ello debes crear un botón `ChangeQuantityButton` para cada uno de los botones.
+- El componente `ChangeQuantityButton` debe recibir dos props: `label` y `onClick`.
 
+```jsx
+import React from "react";
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+        <div> - </div>
+        <div>{tree} tree</div>
+        <div> + </div>
+      <div>Plant trees €2.29</div>
+    </div>
+  );
+}
+```
+*/
+
+import React, { useState } from "react";
+import "./App.css";
+
+function ChangeQuantityButton({ label, onClick }) {
+  return (
+    <div className="button" type="button" onClick={onClick}>
+      {label}
+    </div>
+  );
 }
 
-export default App
+export default function App() {
+  const [tree, setTree] = useState(0);
+
+  function handleDecrease() {
+    setTree((prev) => {
+      if (prev - 1 < 1) {
+        return prev;
+      }
+
+      return prev - 1;
+    });
+  }
+
+  function handleIncrease() {
+    setTree((prev) => prev + 1);
+  }
+
+  return (
+    <div className="App">
+      <div className="top-panel">
+        <ChangeQuantityButton label="-" onClick={handleDecrease} />
+        <div>{tree} tree</div>
+        <ChangeQuantityButton label="+" onClick={handleIncrease} />
+      </div>
+      <div className="bottom-panel">Plant trees €2.29</div>
+    </div>
+  );
+}
